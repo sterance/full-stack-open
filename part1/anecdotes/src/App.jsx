3abedfc58b60f6e5d43eec0262ed9e97bccd8cfa
sterance <smithc9083@gmail.com>
2025-04-07 15:56:39 +1000

    anecdotes step 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+const Heading = ({ text }) => <h1>{text}</h1>
+
 const Button = ({ onClick, text}) => <button onClick={onClick}>{text}</button>;
 
 const Anecdote = ({ text, votes }) => (
@@ -22,7 +24,7 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
   
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
 
   const initialVotes = Array(anecdotes.length).fill(0);
   const [votes, setVotes] = useState(initialVotes);
@@ -38,14 +40,23 @@ const App = () => {
   const incrementVote = () => {
     const newVotes = [...votes];
     newVotes[selected] += 1;
-    setVotes(newVotes)
+    setVotes(newVotes);
   }
+
+
+  let winner = -1;
+  const maxValue = Math.max(...votes);
+  winner = votes.indexOf(maxValue);
+
 
   return (
     <div>
+      <Heading text='Anecdote of the day'/>
       <Anecdote text={anecdotes[selected]} votes={votes[selected]}/>
       <Button onClick={incrementVote} text='vote'/>
       <Button onClick={getRandom} text='next anecdote'/>
+      <Heading text='Anecdote with the most votes'/>
+      <Anecdote text={anecdotes[winner]} votes={votes[winner]}/>
     </div>
   )
 }
