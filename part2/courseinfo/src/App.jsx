3e7bbe08b5bdc1f969f7bfa56cff74a1f962d4f1
sterance <smithc9083@gmail.com>
2025-04-07 17:59:07 +1000

    course info step 9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,42 +1,64 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
-}
-
-const Course = ({ course }) => {
   return (
     <div>
-      <Header text={course.name}/>
-      <Content parts={course.parts}/>
+      {courses.map(course =>
+        <Course key={course.id} course={course}/>
+      )}
     </div>
-  )
+  ) 
 }
+
+const Course = ({ course }) => (
+  <div>
+    <Header text={course.name} />
+    <Content parts={course.parts} />
+  </div>
+);
 
 const Header = ({ text }) => <h1>{text}</h1>
 
@@ -53,18 +75,16 @@ const Content = ({ parts }) => {
   )
 }
 
-const Part = ({ part }) => {
-  return (
-    <div>
-      {part.name} {part.exercises}
-    </div>
-  )
-}
+const Part = ({ part }) => (
+  <div>
+    {part.name} {part.exercises}
+  </div>
+);
 
-const Total = ({ total }) => {
-  return (
-    <div><b>total of {total} excercises</b></div>
-  )
-}
+const Total = ({ total }) => (
+  <div>
+    <strong>Total of {total} exercises</strong>
+  </div>
+);
 
 export default App
