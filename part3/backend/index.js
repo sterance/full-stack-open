ca43e9cb9e99e32e9e42a6f77b5bff7ee5540216
sterance <smithc9083@gmail.com>
2025-04-29 20:34:40 +1000

    phonebook backend step 7

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
+app.use(express.json());
+app.use(requestLogger);
 
 let notes = [
   {
@@ -20,6 +21,14 @@ let notes = [
     important: true
   }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+}
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -74,7 +83,11 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
+app.use(unknownEndpoint)
 
 
 const PORT = 3001
